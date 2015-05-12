bbe73a3a9bc78726a6f5bb719c15363eae657f4a
Steven Grissom <steve.ctr.grissom@faa.gov>
2015-05-12 14:08:35 -0500

    change emacs zip file to a variable and delete it once it's been unzipped.

--- bin/installEmacs.ps1
+++ bin/installEmacs.ps1
@@ -1,19 +1,23 @@
 
 $emacsURL = 'http://ftp.kaist.ac.kr/gnu/gnu/emacs/windows/emacs-24.5-bin-i686-mingw32.zip'
 $emacsPath = $env:ProgramW6432 + '\emacs'
+$emacsZip = 'w:\devenv\bin\emacs.zip'
 
 if (-NOT (Test-Path $emacsPath)){
 
-	mkdir $emacsPath
+    mkdir $emacsPath
 
-	if (-NOT (Test-Path w:\devenv\bin\emacs.zip)){
-		Invoke-WebRequest $emacsURL -OutFile emacs.zip
-	}
+    if (-NOT (Test-Path $emacsZip)){
+	Invoke-WebRequest $emacsURL -OutFile emacs.zip
+    }
 
-	$ws = new-object -com shell.application
-	$zip = $ws.NameSpace("w:\devenv\bin\emacs.zip")
-	foreach($item in $zip.items())
-	{
- 	   $ws.Namespace($emacsPath).copyhere($item)
-	}
+    $ws = new-object -com shell.application
+    $zip = $ws.NameSpace($emacsZip)
+    foreach($item in $zip.items())
+    {
+ 	$ws.Namespace($emacsPath).copyhere($item)
+    }
+
+    Remove-Item -force $emacsZip
+
 }
