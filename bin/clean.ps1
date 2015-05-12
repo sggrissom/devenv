7ee5aec96b27bc037e46f496b4cf9596b5cb52b4
Steven Grissom <steve.ctr.grissom@faa.gov>
2015-05-12 14:07:14 -0500

    no longer delete the emacs install. Test if W: is subst or not before trying to delete it. properly clean up the other changes config has made.

--- bin/clean.ps1
+++ bin/clean.ps1
@@ -1,25 +1,22 @@
 
 $startupFolder = $env:APPDATA + "\Microsoft\Windows\Start Menu\Programs\Startup"
-$startupBatchPath = $startupFolder + "\startup.bat"
+$startupBatchPath = $startupFolder + "\startupLink.bat"
 $desktopFolder = $env:USERPROFILE + "\Desktop"
 $cmdLink = $desktopFolder + "\cmd.lnk"
 $emacsConfigLink = "w:\.emacs.d"
-$emacsPath = $env:ProgramW6432 + '\emacs'
 
 if (Test-Path $emacsConfigLink){
     cmd /c rmdir $emacsConfigLink
 }
 
-cmd /c subst /d w:
+if (Test-Path w:\){
+	subst /d w:
+}
 
 if (Test-Path $startupBatchPath){
-    cmd /c rmdir $startupBatchPath
+    Remove-Item -force $startupBatchPath
 }
 
 if (Test-Path $cmdLink){
     Remove-Item -force $cmdLink
-}
-
-if (Test-Path $emacsPath){
-    Remove-Item -force -recurse $emacsPath
 }
