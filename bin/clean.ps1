c8c225c0ba758be84660e8a5c2fad15ed1f8f98a
Steven Grissom <steve.ctr.grissom@faa.gov>
2015-05-12 10:07:53 -0500

    update the clean script to delete the emacs folder (but not the emacs archive). On second thought I may want to leave it there, but for now while I test the configuration then I want it deleted.

--- bin/clean.ps1
+++ bin/clean.ps1
@@ -4,6 +4,7 @@ $startupBatchPath = $startupFolder + "\startup.bat"
 $desktopFolder = $env:USERPROFILE + "\Desktop"
 $cmdLink = $desktopFolder + "\cmd.lnk"
 $emacsConfigLink = "w:\.emacs.d"
+$emacsPath = $env:ProgramW6432 + '\emacs'
 
 if (Test-Path $emacsConfigLink){
     cmd /c rmdir $emacsConfigLink
@@ -12,10 +13,13 @@ if (Test-Path $emacsConfigLink){
 cmd /c subst /d w:
 
 if (Test-Path $startupBatchPath){
-    Remove-Item $startupBatchPath
+    cmd /c rmdir $startupBatchPath
 }
 
 if (Test-Path $cmdLink){
-    Remove-Item $cmdLink
+    Remove-Item -force $cmdLink
 }
 
+if (Test-Path $emacsPath){
+    Remove-Item -force -recurse $emacsPath
+}
