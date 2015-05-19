53128f19fbae4b8f5b9cfb0087a2fc1ead93b478
unknown <steve ctr grissom@JAMCXWL00106656.amc.faa.gov>
2015-05-19 15:51:54 -0500

    standardized the keybinding calls instead of using the seven different ways that are in here currently. Also added the quick movement keybindings.

--- .emacs.d/sg-keybindings.el
+++ .emacs.d/sg-keybindings.el
@@ -1,34 +1,44 @@
 ;; sg-keybindings.el
 ;; customize key commands
 
+;; view function current keybining
+;; C-h f function-name
+
+;; keybinding current function
+;; C-h k key-sequence
+
 ;;prevent the dumb C-x C-b buffer list opening in a new window and
 ;;instead open it in the same window
-(global-set-key "\C-x\C-b" 'buffer-menu)
+(global-set-key (kbd "C-x C-b") 'buffer-menu)
 
 ;;unset C-x f because why would I care to set the width of whatever?
-(global-unset-key "\C-xf")
-(global-unset-key "\C-c\C-b")
+(global-unset-key (kbd "C-x f"))
+(global-unset-key (kbd "C-c C-b"))
 
 ;; run compilation file
 ;; meta m
-(define-key global-map "\em" 'make-without-asking)
+(global-set-key (kbd "M-m") 'make-without-asking)
 
 ;; open/create file 
 ;; meta f, meta shift f
-(define-key global-map "\ef" 'find-file)
-(define-key global-map "\eF" 'find-file-other-window)
+(global-set-key (kbd "M-f") 'find-file)
+(global-set-key (kbd "M-F") 'find-file-other-window)
 
 ;; switch to file
 ;; meta b, meta shift b
-(global-set-key (read-kbd-macro "\eb")  'ido-switch-buffer)
-(global-set-key (read-kbd-macro "\eB")  'ido-switch-buffer-other-window)
+(global-set-key (kbd "M-b")  'ido-switch-buffer)
+(global-set-key (kbd "M-B")  'ido-switch-buffer-other-window)
 
 ;; switch to other window
 ;; meta w
-(define-key global-map "\ew" 'other-window)
+(global-set-key (kbd "M-w") 'other-window)
 
-(define-key global-map "\eW" 'buf-swap)
+(global-set-key (kbd "M-W") 'buf-swap)
 
 ;; save
 ;; meta s
-(define-key global-map "\es" 'save-buffer)
+(global-set-key (kbd "M-s") 'save-buffer)
+
+;;movement
+(global-set-key (kbd "C-j") 'next-blank-line)
+(global-set-key (kbd "C-k") 'previous-blank-line)
