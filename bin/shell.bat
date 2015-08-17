d8dea75c72dbf67e88708f8bd3e7e3153600f919
unknown <Steve CTR Grissom@JAMCXWL00106656.amc.faa.gov>
2015-08-17 09:40:06 -0500

    don't automatically run vcvarsall when starting a cmd promt, instead create a 'vars' command to run it

--- bin/shell.bat
+++ bin/shell.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set path=w:\bin;w:\devenv\bin;%path%
+set path=w:\bin;w:\devenv\bin;C:\miktex\miktex\bin\x64;%path%
 set home=w:\
+set dt=%userprofile%\Desktop\
 
-call "%ProgramFiles(x86)%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64
---
+++ bin/vars.bat
@@ -0,0 +1,3 @@
+@echo off
+
+call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
