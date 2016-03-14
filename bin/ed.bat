41ea71b6934f7cfe542b37399214c9015797ff5a
Steven Grissom <sggrissom@gmail.com>
2016-03-14 14:34:15 -0500

    Adding a universal alias for opening whatever editor I happen to be using (and in this case I can avoid typing 4...). Also updating my path to play nice with some commands needed for resource compiling.

---
+++ bin/ed.bat
@@ -0,0 +1,3 @@
+@echo off
+
+start 4ed
--- bin/shell.bat
+++ bin/shell.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set path=C:\Program Files\cmder\vendor\msysgit\bin;w:\bin;w:\devenv\bin;%path%
+set path=C:\Program Files\cmder\vendor\msysgit\bin;w:\bin;w:\devenv\bin;%path%;C:\Program Files (x86)\Windows Kits\8.0\bin\x86;C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6 Tools\
 set home=w:\
 set dt=%userprofile%\Desktop\
 set include=w:\slib;%include%
